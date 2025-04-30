31104f2d83ccf92b15efa800a3ef5583cbfcde5f
Sajib <sajibcse14@gmail.com>
2025-04-30 13:11:31 +0600

    Project :Initialized

---
+++ src/main/java/com/buggers/exceptionhandling/service/UserService.java
@@ -0,0 +1,18 @@
+package com.buggers.exceptionhandling.service;
+
+
+
+import com.buggers.exceptionhandling.dto.User;
+
+import java.util.List;
+
+
+public interface UserService {
+    User addUser(User user);
+
+    List<User> getUsers();
+
+    boolean updateUser(String userId, User user);
+
+    boolean deleteUser(String userId);
+}
